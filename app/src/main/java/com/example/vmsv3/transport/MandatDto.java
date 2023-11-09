eb7e70f151860ad75cba53cfa865634f59724e88
radektomczyk <radek05654@gmail.com>
2023-11-09 20:01:06 +0100

    Ticket data parsing

--- app/src/main/java/com/example/vmsv3/transport/MandatDto.java
+++ app/src/main/java/com/example/vmsv3/transport/MandatDto.java
@@ -4,6 +4,7 @@ package com.example.vmsv3.transport;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.LocalDate;
+import java.util.Date;
 
 public class MandatDto {
     @JsonProperty("nazwa")
@@ -13,15 +14,18 @@ public class MandatDto {
     @JsonProperty("waznosc")
     private final int validityMonths;
     @JsonProperty("data_wystawienia")
-    private final LocalDate receiveDate;
+    private final Date receiveDate;
     @JsonProperty("cena")
     private final int amount;
+    @JsonProperty("id_user")
+    private final int id;
 
-    public MandatDto(String reason, int penaltyPoints, int validityMonths, LocalDate receiveDate, int amount) {
+    public MandatDto(String reason, int penaltyPoints, int validityMonths, Date receiveDate, int amount, int id) {
         this.reason = reason;
         this.penaltyPoints = penaltyPoints;
         this.validityMonths = validityMonths;
         this.receiveDate = receiveDate;
         this.amount = amount;
+        this.id = id;
     }
 }
--- app/src/main/java/com/example/vmsv3/ui/add_ticket/AddTicketFragment.java
+++ app/src/main/java/com/example/vmsv3/ui/add_ticket/AddTicketFragment.java
@@ -1,71 +1,112 @@
 package com.example.vmsv3.ui.add_ticket;
 
-import androidx.lifecycle.ViewModelProvider;
-
+import androidx.fragment.app.Fragment;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.vmsv3.R;
 import com.example.vmsv3.databinding.FragmentAddTicketBinding;
+import com.example.vmsv3.service.SamochodioClient;
+import com.example.vmsv3.transport.MandatDto;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class AddTicketFragment extends Fragment {
     private FragmentAddTicketBinding binding;
-    private EditText ticketDate;
+    private EditText reasonEditText;
+    private EditText penaltyPointsEditText;
+    private EditText durationEditText;
+    private EditText dateEditText;
+    private EditText costEditText;
+    private Button button;
 
-
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container,
-                             Bundle savedInstanceState) {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentAddTicketBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        // init calendar view
-        ticketDate = root.findViewById(R.id.ticketDateEditText);
-        ticketDate.setOnClickListener(v -> showDatePicker());
-        Button saveButton = root.findViewById(R.id.saveTicketButton);
-        saveButton.setOnClickListener(this::dupaBiskupa);
+        reasonEditText = root.findViewById(R.id.ticketNameEditText);
+        penaltyPointsEditText = root.findViewById(R.id.penaltyPointsEditText);
+        durationEditText = root.findViewById(R.id.ticketDurationEditText);
+        dateEditText = root.findViewById(R.id.ticketDateEditText);
+        costEditText = root.findViewById(R.id.ticketAmountEditText);
+        button = root.findViewById(R.id.saveTicketButton);
+
+        // Initialize calendar view
+        dateEditText.setOnClickListener(v -> showDatePicker());
+
+        button.setOnClickListener(this::createTicket);
 
         return root;
     }
 
-    private void dupaBiskupa(View view) {
-        String authToken = "dupabiskupa";
-//        MandatDto mandatDto = new MandatDto(" ", 2, 12);
-//        SamochodioClient.getInstance().createTicket(mandatDto, authToken);
+    private void createTicket(View view) {
+        // Retrieve input values
+        String authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibG9naW4iOiJydG9tY3p5ayIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTY5OTQ0NzI5OSwiZXhwIjoxNjk5NTMzNjk5fQ.-7n2qlSEvKNpOcDzp1Q9czKbtXw7ecLWIkJATR8jr7I";
+        String reason = reasonEditText.getText().toString();
+        int penaltyPoints = parseEditTextToInt(penaltyPointsEditText);
+        int duration = parseEditTextToInt(durationEditText);
+        Date date = parseDateEditText();
+        int cost = parseEditTextToInt(costEditText);
+
+        // Create MandatDto
+        MandatDto mandatDto = new MandatDto(reason, penaltyPoints, duration, date, cost, 1);
+
+        // Call API to create ticket
+        SamochodioClient.getInstance().createTicket(mandatDto, authToken);
+    }
+
+    private Date parseDateEditText() {
+        String dateString = dateEditText.getText().toString();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        try {
+            return dateFormat.parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            Toast.makeText(getContext(), "Invalid date format", Toast.LENGTH_SHORT).show();
+            return new Date();
+        }
+    }
+
+    private int parseEditTextToInt(EditText editText) {
+        try {
+            return Integer.parseInt(editText.getText().toString());
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            Toast.makeText(getContext(), "Invalid number format", Toast.LENGTH_SHORT).show();
+            return 0;
+        }
+    }
+
+    private void showDatePicker() {
+        // Get current date
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        // Create DatePickerDialog
+        DatePickerDialog datePickerDialog = new DatePickerDialog(requireContext(), (view, year1, month1, dayOfMonth) ->
+                dateEditText.setText(String.format(Locale.getDefault(), "%d/%02d/%02d", year1, month1 + 1, dayOfMonth)), year, month, day);
+
+        // Display DatePickerDialog
+        datePickerDialog.show();
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
-    }
-    private void showDatePicker() {
-        // Pobierz aktualną datę
-        Calendar calendar = Calendar.getInstance();
-        int year = calendar.get(Calendar.YEAR);
-        int month = calendar.get(Calendar.MONTH);
-        int day = calendar.get(Calendar.DAY_OF_MONTH);
-
-        // Utwórz DatePickerDialog
-        DatePickerDialog datePickerDialog = new DatePickerDialog(requireContext(), new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                ticketDate.setText(dayOfMonth + "/" + (month + 1) + "/" + year);
-            }
-        }, year, month, day);
-
-        // Wyświetl DatePickerDialog
-        datePickerDialog.show();
     }
 }
